6262d4f7403bc55da22936dba6be65ccab12a1df
Takashi Hachinohe <takashi.hachinohe@gmail.com>
2014-01-16 11:43:43 +0900

    JUnitのソースは削除。

---
+++ src/test/scala/HelloSuite.scala
@@ -0,0 +1,15 @@
+import org.scalatest.FunSuite
+
+class HelloSuite extends FunSuite {
+
+  test("引数を指定しない場合に\"Hello!\"が取得できる") {
+    val actual = Hello.createGreeting(Array())
+    assert(actual == "Hello!")
+  }
+
+  test("引数に\"Takashi\"を指定した場合に\"Hello Takashi!\"が取得できる") {
+    val actual = Hello.createGreeting(Array("Takashi"))
+    assert(actual == "Hello Takashi!")
+  }
+
+}
