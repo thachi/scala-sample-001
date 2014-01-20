dab6a31d1df3d94db7bdaf5a92ac1b2061562e23
Takashi Hachinohe <takashi.hachinohe@gmail.com>
2014-01-21 02:07:17 +0900

    ScalaLoggingをLog4j2で使うようにしてみました。

--- build.sbt
+++ build.sbt
@@ -4,5 +4,9 @@ version := "1.0"
 
 scalaVersion := "2.10.3"
 
-libraryDependencies += "org.scalatest" % "scalatest_2.10" % "2.0" % "test"
+libraryDependencies ++= Seq(
+  "com.typesafe" %% "scalalogging-log4j" % "1.0.1",
+  "org.apache.logging.log4j" % "log4j-core" % "2.0-beta9",
+  "org.scalatest" % "scalatest_2.10" % "2.0" % "test"
+)
 
--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,12 +1,17 @@
 package example
 
-object Hello {
+import com.typesafe.scalalogging.log4j.Logging
+
+object Hello extends Logging {
 
   def main(args: Array[String]) {
     println(createGreeting(args))
   }
 
   def createGreeting(args: Array[String]) = {
+
+    logger.info("args size:" + args.length)
+
     args.headOption match {
       case Some(name) => "Hello " + name + "!"
       case None => "Hello!"
